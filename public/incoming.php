8feea19794dfe7bc5a51f9c60e7023af4de4a899
Paul Leduc <leduc.paul@gmail.com>
2015-03-25 22:24:01 -0700

    added better error reporting

--- public/incoming.php
+++ public/incoming.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 $scavengerHunt = new Twilio_Scavenger_Hunt();
 
 $this->setIvrText("Hello! Welcome to the unltimate scavenger hunt! Text the word start to this phone number to begin!");
