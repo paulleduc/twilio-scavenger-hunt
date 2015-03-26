26ab1424e8f9552be396b28a31afa2ab13308a63
Paul Leduc <leduc.paul@gmail.com>
2015-03-25 22:28:39 -0700

    quick typo fix

--- public/incoming.php
+++ public/incoming.php
@@ -10,7 +10,7 @@ require_once('../includes/Twilio_Scavenger_Hunt_Stage.php');
 
 $scavengerHunt = new Twilio_Scavenger_Hunt();
 
-$this->setIvrText("Hello! Welcome to the unltimate scavenger hunt! Text the word start to this phone number to begin!");
+$scavengerHunt->setIvrText("Hello! Welcome to the unltimate scavenger hunt! Text the word start to this phone number to begin!");
 
 $scavengerHunt->addStage("start", "I open at the close1.", "In your closet.", "Crossword Clue: 6 Across: A strange smell");
 $scavengerHunt->addStage("two", "I open at the close2.", "In your closet.", "Crossword Clue: 6 Across: A strange smell");
